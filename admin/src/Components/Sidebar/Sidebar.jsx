8e2bf883fa35809633d299ad436421bfe051115c
Nelson Pineda <npinedac3@miumge.edu.gt>
2024-04-07 22:22:18 -0600

    agregando el ultimo caso de jsx

--- admin/src/Components/Sidebar/Sidebar.jsx
+++ admin/src/Components/Sidebar/Sidebar.jsx
@@ -1,2 +1,31 @@
+import React from 'react'
+import './Sidebar.css'
+import {Link} from 'react-router-dom'
+import add_product_icon from '../../assets/Product_Cart.svg'
+import list_product_icon from  '../../assets/Product_list_icon.svg'
+
+
+const Sidebar = () => {
+    return (
+        <div className= 'sidebar'>
+            <Link to= {'/addproduct'} style={{textDecoration:"none"}}>
+                <div className="sidebar-item">
+                    <img src={add_product_icon} alt=""/>
+                    <p>Add Product</>
+                </div>
+            </Link> 
+            <Link to= {'/listprodcut'} style={{textDecoration:"none"}}>
+                <div className="sidebar-item">
+                    <img src={list_product_icon} alt=""/>
+                    <p>Product List</>
+                </div>
+            </Link>
+        </div>
+)
+
+
+
+}
+export default Sidebar
 6:11:07 - 6:11:47
 6:12:35 - 6:16:07
